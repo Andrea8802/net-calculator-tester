cde02194bb76ecd7bde131a15de339cd96d48014
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-26 16:36:50 +0200

    Fixata ripetizione test

--- TestCalculator/CalculatorTest.cs
+++ TestCalculator/CalculatorTest.cs
@@ -19,9 +19,7 @@ namespace TestCalculator
             Assert.That(Calculator.Add(num1, num2), Is.EqualTo(10f));
         }
 
-        [Test]
         [TestCase(3.23f, 3.77f)]
-
         public void AddKO(float num1, float num2)
         {
             Assert.That(Calculator.Add(num1, num2), Is.Not.EqualTo(10f));
@@ -31,14 +29,13 @@ namespace TestCalculator
         // Sottrazione
         [Test]
         [TestCase(6.23f, 3.77f)]
+
         public void SubstractOK(float num1, float num2)
         {
             Assert.That(Calculator.Substract(num1, num2), Is.EqualTo(2.46f));
         }
 
-        [Test]
         [TestCase(3.23f, 3.77f)]
-
         public void SubstractKO(float num1, float num2)
         {
             Assert.That(Calculator.Substract(num1, num2), Is.Not.EqualTo(2.46f));
@@ -48,20 +45,20 @@ namespace TestCalculator
         // Divisione
         [Test]
         [TestCase(2.5f, 5f)]
+
+
         public void DivideOK(float num1, float num2)
         {
             Assert.That(Calculator.Divide(num1, num2), Is.EqualTo(0.5f));
 
         }
 
-        [Test]
         [TestCase(2.1f, 5.3f)]
         public void DivideKO(float num1, float num2)
         {
             Assert.That(Calculator.Divide(num1, num2), Is.Not.EqualTo(0.5f));
         }
 
-        [Test]
         [TestCase(0f, 0f)]
         public void DivideError(float num1, float num2)
         {
@@ -72,14 +69,13 @@ namespace TestCalculator
         // Moltiplicazione
         [Test]
         [TestCase(2.5f, 7.5f)]
+
         public void MultiplyOK(float num1, float num2)
         {
             Assert.That(Calculator.Multiply(num1, num2), Is.EqualTo(18.75f));
         }
 
-        [Test]
         [TestCase(3.5f, 7.2f)]
-
         public void MultiplyKO(float num1, float num2)
         {
             Assert.That(Calculator.Multiply(num1, num2), Is.Not.EqualTo(18.75f));
@@ -89,15 +85,12 @@ namespace TestCalculator
         // Elevamento a potenza
         [Test]
         [TestCase(2.3f, 2f)]
-
         public void ElevaOK(float numBase, float esponente)
         {
             Assert.That(Calculator.Eleva(numBase, esponente), Is.EqualTo(5.29f));
         }
 
-        [Test]
         [TestCase(2.8f, 4.2f)]
-
         public void ElevaKO(float numBase, float esponente)
         {
             Assert.That(Calculator.Eleva(numBase, esponente), Is.Not.EqualTo(5.29f));
